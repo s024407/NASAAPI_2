ef4abbbaf71337d59af3c646e514fe53e186118c
Lucy Thoen <s019409@students.lmsd.org>
2024-12-17 15:13:16 -0500

    hello all

--- NASAAPI/ContentView.swift
+++ NASAAPI/ContentView.swift
@@ -13,7 +13,7 @@ struct ContentView: View {
             Image(systemName: "globe")
                 .imageScale(.large)
                 .foregroundStyle(.tint)
-            Text("Hello, planet!")
+            Text("Hello, all!")
         }
         .padding()
     }
