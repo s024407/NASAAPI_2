03ca471eda313a7188ccd36e8b05620a5f3428dd
Griffin Schlabach <griffin.schlabach@gmail.com>
2025-01-02 15:06:47 -0500

    allows access to data in imageview and contentview

--- NASAAPI/ContentView.swift
+++ NASAAPI/ContentView.swift
@@ -8,14 +8,11 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State var data = FetchData()
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, all!")
-        }
-        .padding()
+        ImageView(data:$data)
     }
 }
 
