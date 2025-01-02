16b27cd50f138513b678d282d241a073a36295ce
Allison Tan <s024407@students.lmsd.org>
2025-01-02 14:49:06 -0500

    constants file

--- NASAAPI/Constants.swift
+++ NASAAPI/Constants.swift
@@ -5,4 +5,16 @@
 //  Created by Allison Tan (student LM) on 1/2/25.
 //
 
-import Foundation
+import SwiftUI
+
+struct Constants{
+    static let headerText: Font = Font(UIFont(name: "Helvetica-Neue Bold", size: 40) ?? UIFont.systemFont(ofSize: 40))
+    static let bodyText: Font = Font(UIFont(name: "Helvetica-Neue", size: 30) ?? UIFont.systemFont(ofSize: 30))
+}
+
+extension Color{
+    static let backgroundBlue = Color("background")
+    static let textBlue = Color("text")
+
+}
+
