b890dedbc7ca5f2d611822fbb8bdc7758d023a00
Lucy Thoen <s019409@students.lmsd.org>
2025-01-02 15:13:40 -0500

    datepicker

--- NASAAPI/DatesView.swift
+++ NASAAPI/DatesView.swift
@@ -8,8 +8,14 @@
 import SwiftUI
 
 struct DatesView: View {
+    
+    @State private var date = Date()
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        DatePicker("Date", selection: $date, displayedComponents: [.date])
+            .datePickerStyle(.graphical)
+        
+//        print(date.formatted())
     }
 }
 
