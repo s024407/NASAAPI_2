07448e80f57058f60d273aaefd11e8ac48a62a19
Griffin Schlabach <griffin.schlabach@gmail.com>
2025-01-07 14:01:54 -0500

    Fixed FetchData and began scrollview

--- NASAAPI/FetchData.swift
+++ NASAAPI/FetchData.swift
@@ -9,23 +9,23 @@ import Foundation
 
 struct FetchData {
     
-    var response:Response = Response()
+    var response:[Photo] = []
     
     mutating func getData() async {
         
-        let URLString = "https://api.nasa.gov/planetary/apod?start_date=2024-12-24&end_date=2024-12-31&api_key=l83gOnig610oKVnBAZIT33oL2oPlWMAOMC51aQ5T"
+        let URLString = "https://api.nasa.gov/planetary/apod?start_date=2024-12-01&end_date=2024-12-31&api_key=l83gOnig610oKVnBAZIT33oL2oPlWMAOMC51aQ5T"
         
         guard let url = URL(string:URLString) else {return}
         guard let (data, _) = try? await URLSession.shared.data(from:url) else {return}
         
-        guard let r = try? JSONDecoder().decode(Response.self, from:data) else {return}
+        guard let r = try? JSONDecoder().decode([Photo].self, from:data) else {return}
         response = r
         
     }
 }
 
 struct Response: Codable {
-    var photos:[Photo] = []
+    var photos: [Photo] = []
 }
 
 struct Photo: Codable {
@@ -37,12 +37,7 @@ struct Photo: Codable {
 
 extension Photo : Identifiable {
     var id:String {url ?? " "}
-    var urlToImage:URL? {
-        if let imgSrc = url {
-            return URL(string: imgSrc)
-        }
-        return nil
-    }
+    
     
 }
 
