6743c7af45ac0f3fb7f1bab72ad082937523e7ac
Allison Tan <s024407@students.lmsd.org>
2025-01-08 15:14:03 -0500

    imageview

--- NASAAPI/ImageView.swift
+++ NASAAPI/ImageView.swift
@@ -13,27 +13,52 @@ struct ImageView: View {
     @Binding var photo: Photo
     
     var body: some View {
-            AsyncImage(url: URL(string: photo.url ?? " ")) {
-                phase in
-                switch phase {
-                case .empty:
-                    ProgressView()
-                case .success(let image):
-                    image
-                        .resizable()
-                        .aspectRatio(contentMode: .fit)
-                case .failure(_):
-                    Image("fnf")
-                        .resizable()
-                        .aspectRatio(contentMode: .fit)
-                    
-                @unknown default:
-                    fatalError()
-                }
+        ZStack {
+            Rectangle()
+                .foregroundColor(.backgroundBlue)
+                .scaledToFill()
+                .frame(width: UIScreen.main.bounds.size.width, height: UIScreen.main.bounds.size.height)
+                .clipped()
+                .ignoresSafeArea()
+
+            VStack {
+                Text(photo.title ?? "[Title]")
+                    .font(Constants.headerText)
+                    .foregroundColor(.textBlue)
+                .bold()
+                Text(photo.date ?? "[Date]")
+                    .font(Constants.bodyText)
+                    .foregroundColor(.textBlue)
+                    AsyncImage(url: URL(string: photo.url ?? " ")) {
+                        phase in
+                        switch phase {
+                        case .empty:
+                            ProgressView()
+                        case .success(let image):
+                            image
+                                .resizable()
+                                .aspectRatio(contentMode: .fit)
+                                .padding(10)
+                                .foregroundColor(.textBlue)
+                        case .failure(_):
+                            Image("fnf")
+                                .resizable()
+                                .aspectRatio(contentMode: .fit)
+                                .padding(10)
+                                .foregroundColor(.textBlue)
+                            
+                        @unknown default:
+                            fatalError()
+                        }
+                    }
             }
+        }
+        
+        
     }
 }
 
 #Preview {
     ImageView(photo: .constant(Photo()))
 }
+
